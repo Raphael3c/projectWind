6799f8ecb43d8e171b002d214f47379255fde2a5
Raphael3c <phael.ufc@gmail.com>
2021-03-31 19:35:26 -0300

    Construcao do Endpoint /game

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser')
+dotenv.config();
+
+const conect = require('./models/index');
+
 const gamesRouter = require('./routes/games');
 
 const app = express();
+
+app.use(express.urlencoded({extend: false}))
+app.use(express.json())
+app.use(cookieParser())
+conect();
 
 app.get('/', (req, res) => {
     return res.json({mensage: 'api ok'})
--- src/routes/games.js
+++ src/routes/games.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const Games = require('../models/Games');
+
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    return res.json({mensage: 'games ok!'})
+const DEFAULT_LIMIT = Number(process.env.DEFAULT_LIMIT);
+const DEFAULT_PAGE = Number(process.env.DEFAULT_PAGE);
+
+router.get('/', async (req, res) => {
+
+    const {limit, page, fields, orderBy, sortBy, q} = req.query;
+
+    const criteria = {
+        limit: Number(limit) || DEFAULT_LIMIT,
+        page: Number(page) || DEFAULT_PAGE,
+        fields: fields || null,
+        orderBy: orderBy || 'title',
+        sortyBy: sortBy !== undefined ? Number(sortBy) : 1, 
+        q: q || '',
+    }
+
+    const result = await Games.find(criteria);
+
+    return res.json({mensage: 'games ok!', data: result})
 })
 
 
